47d72a318b7616bf9fc011f78f6e2d6621d33e0a
Bilal22992 <haiderbilal22992@gmail.com>
2025-02-14 22:33:39 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import bodyParser from "body-parser";
+import express from "express";
+
+const app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.listen(3000,()=>{
+    console.log("server running successfully");
+})
+
+
+app.get("/", async(req,res)=>{
+    try {
+        
+        // const response = await axios.get("https://api.openweathermap.org/data/2.5/weather?lat=44.34&lon=10.99",{params:{appid:"5c72e798f3fc801d33823ffd0906d10b"}});
+        // console.log(response.data);
+
+    } catch (error) {
+        console.log(error.message)
+    }
+})
